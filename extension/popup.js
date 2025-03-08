8c17202b3fa405fced9812884e69646bfcd7642a
Delikinesis <kirill.delikatnyi@lis.ac.uk>
2025-03-08 17:28:25 +0000

    Update popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -1,1 +1,5 @@
-
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === "result") {
+    document.getElementById("result").textContent = request.data;
+  }
+});
