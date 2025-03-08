fe92789f35577b141562d9023807b6f8291e5296
Delikinesis <kirill.delikatnyi@lis.ac.uk>
2025-03-08 17:31:15 +0000

    Delete popup.js

--- popup.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === "result") {
-    document.getElementById("result").textContent = request.data;
-  }
-});
