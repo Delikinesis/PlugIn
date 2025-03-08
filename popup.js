b0ede34214772955d6a785c7f23bc9f53102b0b9
Delikinesis <kirill.delikatnyi@lis.ac.uk>
2025-03-08 17:13:35 +0000

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-// Receive result from background script and update popup UI
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === "result") {
     document.getElementById("result").textContent = request.data;
